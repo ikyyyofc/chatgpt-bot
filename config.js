54cde55258d2f4474513705eff4e4d837b3dcece
ikyyyofc <dikivirtual@gmail.com>
2025-05-03 00:08:02 +0700

    update config.js

--- config.js
+++ config.js
@@ -7,19 +7,5 @@ global.defaultSystemMessages = [
         role: "system",
         content:
             "untuk informasi tentang pembuatmu, dia memiliki beberapa akun seperti wa, ml, tiktok, dan ig. untuk akun wa dia yaitu nomornya 089514509029, kalo ml id nya 345750037, kalo tiktok usernamenya @diki_ikyy, dan kalo ig usernamenya @ikyyofc."
-    },
-    {
-        role: "system",
-        content: `semua respons harus mengikuti format JSON ini:
-
-{ "type": "<tipe_respons>", "input": "<input dari pengguna di sini>", "output": "<respons kamu di sini dan output harus berupa string, jangan pernah menghasilkan output object atau array disini>" }`
-    },
-    {
-        role: "system",
-        content: `kalo pengguna minta lagu, ubah 'type' jadi 'play'. kalo judul lagunya belum jelas, tanyain untuk klarifikasi dan jangan ubah 'type' jadi 'play' sebelum judul lagunya jelas. kalo jelas, isi 'input' dengan judul lagunya dan 'output' diisi dengan respons bahwa lagi nunggu lagu terkirim.`
-    },
-    {
-        role: "system",
-        content: `untuk semua pertanyaan lainnya, pertahankan 'type' sebagai 'text', isi 'input' dengan pertanyaan pengguna, dan berikan respons seperti biasa.`
     }
 ];
