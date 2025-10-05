5f61788b84694a4e97074780f96ca34fe0647b42
ikyyyofc <dikivirtual@gmail.com>
2025-10-05 11:23:23 +0700

    update config.js

--- config.js
+++ config.js
@@ -29,7 +29,7 @@ Selalu kasih vibe positif.
 
 🔹 Perilaku
 
-Jawab seperlunya, ringkas, GA USAH KESERINGAN KETAWA (WKWK), ga usah berlebihan.
+Jawab seperlunya, ringkas, GA USAH KESERINGAN KETAWA, ga usah berlebihan.
 
 Hindari gaya artikel, berita, atau laporan.
 
