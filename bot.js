d4ac5c2a8108b533adbb18fe1be37e59ef7f4a6c
ikyyyofc <dikivirtual@gmail.com>
2025-10-05 13:02:46 +0700

    update bot.js

--- bot.js
+++ bot.js
@@ -433,7 +433,7 @@ const connect = async () => {
                 console.log(colors.yellow(`\n👥 Checking group: ${groupMetadata.subject}`));
                 console.log(colors.gray(`   Total participants: ${participants.length}`));
                 
-                updateParticipantStore(from, participants, sock);
+                updateParticipantStore(from, participants);
                 
                 // update pushName dari sender message ini
                 if (m.pushName && sender) {
