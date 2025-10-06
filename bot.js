832fe0a9ec4d1d31f5ac013b51e42c48a98d2254
ikyyyofc <dikivirtual@gmail.com>
2025-10-07 05:37:50 +0700

    update bot.js

--- bot.js
+++ bot.js
@@ -337,10 +337,9 @@ const connect = async () => {
             sock.sendMessage(config.OWNER_NUMBER + "@s.whatsapp.net", {
                 text: util.inspect(m, { depth: 2 })
             });*/
-        const from = m.key.remoteJid.startsWith("status@broadcast")
-            ? m.key.participant
-            : m.key.remoteJid;
         const isGroup = from.endsWith("@g.us");
+        const isStatus = from.startsWith("status@broadcast");
+        const from = isStatus ? m.key.participant : m.key.remoteJid;
 
         const sender = getSender(m, isGroup, from);
         const senderNumber = sender.split("@")[0];
