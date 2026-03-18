2f84a0b110af1324510ba21b77d9c1a22b690352
ikyyyofc <dikivirtual@gmail.com>
2026-03-18 13:54:54 +0700

    update bot.js

--- bot.js
+++ bot.js
@@ -20,6 +20,7 @@ import { fileURLToPath } from "url";
 import { dirname } from "path";
 import NodeCache from "node-cache";
 import { useSQLiteAuthState } from "./lib/sqliteAuthState.js";
+import { sendButtons, sendInteractiveMessage } from "./lib/button.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
